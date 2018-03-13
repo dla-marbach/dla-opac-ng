dad135c95367e10a06235e72bf342d80d6d8e103
haogatyp <gatermann@effective-webwork.de>
2018-03-13 18:23:14 +0100

    initial list view

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -459,3 +459,23 @@ plugin.tx_find {
 
     }
 }
+
+
+# Localization:
+    config {
+    linkVars = L
+    sys_language_uid = 0
+    sys_language_overlay = 1
+    sys_language_mode = content_fallback
+    language = de
+    locale_all = de_DE.UTF-8
+    htmlTag_setParams = lang="de" dir="ltr" class="no-js"
+}
+[globalVar = GP:L = 1]
+config {
+    sys_language_uid = 1
+    language = en
+    locale_all = en_US.UTF-8
+    htmlTag_setParams = lang="en" dir="ltr" class="no-js"
+}
+[global]
