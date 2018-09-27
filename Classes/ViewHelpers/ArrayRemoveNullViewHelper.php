fec488a0347f4f1f39396750d5e4663c664767a4
Christopher Timm <timm@effective-webwork.de>
2018-09-27 14:54:44 +0200

    adjustments for detailpage "Bilder und Objekte"

---
+++ Classes/ViewHelpers/ArrayRemoveNullViewHelper.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Dla\DlaOpacNg\ViewHelpers;
+
+
+class ArrayRemoveNullViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
+{
+
+
+    /**
+     * Register arguments.
+     * @return void
+     */
+    public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerArgument('array', 'array', 'List of fields', true, array());
+        $this->registerArgument('as', 'string', 'name of the label result variable', true, array());
+    }
+
+    /**
+     * @return boolean
+     */
+    public function render()
+    {
+        $array = $this->arguments['array'];
+        foreach ($array as $key => $value) {
+            if ($value !== NULL) {
+                $resultValue[] = $value;
+            }
+        }
+
+        $valueName = $this->arguments['as'];
+        if ($valueName !== null) {
+            if ($this->templateVariableContainer->exists($valueName)) {
+                $this->templateVariableContainer->remove($valueName);
+            }
+            $this->templateVariableContainer->add($valueName, $resultValue);
+//            $result = $this->renderChildren();
+        }
+
+    }
+}
+?>
