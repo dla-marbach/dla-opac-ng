0d078ba7be576d01c484361ecbf135a8154e2f4e
Christopher Timm <timm@effective-webwork.de>
2018-10-04 12:16:14 +0200

    replace /SP/ and /BR/ to linebreaks

---
+++ Classes/ViewHelpers/ReplaceBreaksViewHelper.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Dla\DlaOpacNg\ViewHelpers;
+
+
+class ReplaceBreaksViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
+{
+
+
+    /**
+     * Register arguments.
+     * @return void
+     */
+    public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerArgument('text', 'string', 'Text', true, 'string');
+        $this->registerArgument('as', 'string', 'name of the label result variable', true, 'string');
+    }
+
+    /**
+     * @return boolean
+     */
+    public function render()
+    {
+        $resultValue = str_replace("/SP/", "<br/>", $this->arguments['text']);
+        $resultValue = str_replace("/BR/", "<br/>", $resultValue);
+
+        if (is_array($resultValue)) {
+            $resultValue = $resultValue[0];
+        }
+
+        $valueName = $this->arguments['as'];
+        if ($valueName !== null) {
+            if ($this->templateVariableContainer->exists($valueName)) {
+                $this->templateVariableContainer->remove($valueName);
+            }
+            $this->templateVariableContainer->add($valueName, $resultValue);
+//            $result = $this->renderChildren();
+        }
+
+    }
+}
+?>
