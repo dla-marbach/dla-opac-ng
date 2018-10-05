4320d1367e2e5e87dca29708fc08bf8eb6618efe
Christopher Timm <timm@effective-webwork.de>
2018-10-05 09:21:57 +0200

    fix date format

---
+++ Classes/ViewHelpers/FormatDateViewHelper.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Dla\DlaOpacNg\ViewHelpers;
+
+
+class FormatDateViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
+{
+
+
+    /**
+     * Register arguments.
+     * @return void
+     */
+    public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerArgument('date', 'string', 'Datum', true, '');
+        $this->registerArgument('regexDay', 'string', 'Regex for input', false, '/\d{4}\d{2}(\d{2})/');
+        $this->registerArgument('regexMonth', 'string', 'Regex for input', false, '/\d{4}(\d{2})\d{2}/');
+        $this->registerArgument('regexYear', 'string', 'Regex for input', false, '/(\d{4})\d{2}\d{2}/');
+        $this->registerArgument('format', 'string', 'Output format', false, '');
+        $this->registerArgument('as', 'string', 'name of the label result variable', true, array());
+    }
+
+    /**
+     * @return boolean
+     */
+    public function render()
+    {
+        $date = $this->arguments['date'];
+        $year = preg_match($this->arguments['regexYear'], $date,$matchYear);
+        $month = preg_match($this->arguments['regexMonth'], $date,$matchMonth);
+        $day = preg_match($this->arguments['regexDay'], $date,$matchDay);
+
+        $yearOnly = false;
+
+        if ($matchDay[1] == "00") {
+            $matchDay[1] = 01;
+            $yearOnly = true;
+        }
+
+        if ($matchMonth[1] == "00") {
+            $matchMonth[1] = 01;
+            $yearOnly = true;
+        }
+
+        $resultValue = new \DateTime();
+        $resultValue->setDate($matchYear[1], $matchMonth[1], $matchDay[1]);
+
+        if ($yearOnly) {
+            $resultValue = date_format($resultValue, 'Y');
+        } else {
+            $resultValue = date_format($resultValue, 'd.m.Y');
+        }
+
+        $valueName = $this->arguments['as'];
+        if ($valueName !== null) {
+            if ($this->templateVariableContainer->exists($valueName)) {
+                $this->templateVariableContainer->remove($valueName);
+            }
+            $this->templateVariableContainer->add($valueName, $resultValue);
+//            $result = $this->renderChildren();
+        }
+
+    }
+}
+?>
