911a789fc0be5f8da057d5c4032f8f04d247edbf
Christopher Timm <timm@effective-webwork.de>
2019-01-24 16:40:45 +0100

    add array merge viewhelper

---
+++ Classes/ViewHelpers/ArrayMergeViewHelper.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Dla\DlaOpacNg\ViewHelpers;
+
+
+class ArrayMergeViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
+{
+
+
+    /**
+     * Register arguments.
+     * @return void
+     */
+    public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerArgument('array1', 'array', 'List of fields', true, array());
+        $this->registerArgument('array2', 'array', 'List of fields', true, array());
+        $this->registerArgument('array3', 'array', 'List of fields', true, array());
+        $this->registerArgument('as', 'string', 'name of the label result variable', true, array());
+    }
+
+    /**
+     * @return boolean
+     */
+    public function render()
+    {
+        $resultValue = array_merge_recursive($this->arguments['array1'], $this->arguments['array2'], $this->arguments['array3']);
+
+        $valueName = $this->arguments['as'];
+        if ($valueName !== null) {
+            if ($this->templateVariableContainer->exists($valueName)) {
+                $this->templateVariableContainer->remove($valueName);
+            }
+            $this->templateVariableContainer->add($valueName, $resultValue);
+//            $result = $this->renderChildren();
+        }
+
+    }
+}
+?>
