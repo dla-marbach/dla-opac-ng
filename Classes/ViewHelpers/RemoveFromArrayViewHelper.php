8b033dcbe097ff1daa9e0a6aab57f050b134aff6
Christopher Timm <timm@effective-webwork.de>
2019-01-28 11:11:00 +0100

    remove rs and #nv outputs

---
+++ Classes/ViewHelpers/RemoveFromArrayViewHelper.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Dla\DlaOpacNg\ViewHelpers;
+
+
+class RemoveFromArrayViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
+{
+
+
+    /**
+     * Register arguments.
+     * @return void
+     */
+    public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerArgument('array', 'array', 'Array', true, null);
+        $this->registerArgument('array2', 'array', 'Array', true, null);
+
+        $this->registerArgument('as', 'string', 'name of the label result variable', true, 'string');
+        $this->registerArgument('as2', 'string', 'name of the label result variable', true, 'string');
+    }
+
+    /**
+     * @return boolean
+     */
+    public function render()
+    {
+        $array = $this->arguments['array'];
+        $array2 = $this->arguments['array2'];
+
+        $newArray = array();
+        $newArray2 = array();
+
+        $i = 0;
+        foreach ($array as $key => $value) {
+            if (!($value == '␞' || $value == '#NV') && !($array2[$i] == '␞' || $array2[$i] == '#NV' )) {
+                $newArray[$i] = $value;
+                $newArray2[$i] = $array2[$i];
+            }
+            $i++;
+        }
+
+        $valueName = $this->arguments['as'];
+        if ($valueName !== null) {
+            if ($this->templateVariableContainer->exists($valueName)) {
+                $this->templateVariableContainer->remove($valueName);
+            }
+            $this->templateVariableContainer->add($valueName, $newArray);
+        }
+
+        $valueName = $this->arguments['as2'];
+        if ($valueName !== null) {
+            if ($this->templateVariableContainer->exists($valueName)) {
+                $this->templateVariableContainer->remove($valueName);
+            }
+            $this->templateVariableContainer->add($valueName, $newArray2);
+        }
+
+    }
+}
+?>
