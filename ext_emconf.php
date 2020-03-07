e5dfa52ba3e968c92a6bf48d05c40aef7fb638a6
Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
2020-03-07 14:09:27 +0100

    Add dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,10 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
+            'php' => '7.2.0-7.3.99',
             'typo3' => '8.7.0-8.7.99',
+            'typo3_console' => '5.0.0-5.9.99',
+            'find' => '3.0.0-3.1.99'
         ],
         'conflicts' => [],
         'suggests' => [],
