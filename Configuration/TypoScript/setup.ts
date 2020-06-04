cfd5c8ff8130b18285689286c0aef8be320e8524
dla-kramski <kramski@dla-marbach.de>
2020-06-04 15:45:28 +0200

    Avoid phrase search in detail_werke_translation_gedrucktes and detail_werke_rezension_*. Add detail_werke_rezension_aundv.

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -503,7 +503,7 @@ plugin.tx_find {
                 id = detail_werke_translation_gedrucktes
                 type = Text
                 # query = (GWKEY:("%1$s") OR AKE526:("%1$s")) AND NOT DOKTYP:("Werktitel" OR "Ton- und Bildträger") AND facet_form_content:("Übersetzung" OR "Übersetzung, deutsch" OR "Übersetzung, fremdsprachig") AND source:("AK")
-                query = ("%1$s") AND NOT DOKTYP:("Werktitel" OR "Ton- und Bildträger") AND facet_form_content:("Übersetzung" OR "Übersetzung, deutsch" OR "Übersetzung, fremdsprachig") AND source:("AK")
+                query = (%1$s) AND NOT DOKTYP:("Werktitel" OR "Ton- und Bildträger") AND facet_form_content:("Übersetzung" OR "Übersetzung, deutsch" OR "Übersetzung, fremdsprachig") AND source:("AK")
                 noescape = 1
                 hidden = 1
             }
@@ -581,14 +581,21 @@ plugin.tx_find {
             155 {
                 id = detail_werke_rezension_gedrucktes
                 type = Text
-                query = ("%1$s") AND facet_form_content:("Rezension") AND source:("AK")
+                query = (%1$s) AND facet_form_content:("Rezension") AND source:("AK") AND NOT DOKTYP:("Werktitel" OR "Ton- und Bildträger")
                 noescape = 1
                 hidden = 1
             }
             156 {
                 id = detail_werke_rezension_handschriften
                 type = Text
-                query = ("%1$s") AND facet_form_content:("Rezension") AND source:("HS")
+                query = (%1$s) AND facet_form_content:("Rezension") AND source:("HS")
+                noescape = 1
+                hidden = 1
+            }            
+            157 {
+                id = detail_werke_rezension_aundv
+                type = Text
+                query = (%1$s) AND facet_form_content:("Rezension") AND DOKTYP:("Ton- und Bildträger")
                 noescape = 1
                 hidden = 1
             }            
