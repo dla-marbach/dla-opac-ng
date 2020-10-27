ddcd1c3f36137cf368cc912f3428a41d30bdb722
Christopher Timm <timm@effective-webwork.de>
2020-10-27 16:59:02 +0100

    add lightbox for detail images

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -77,6 +77,9 @@ page.includeJSFooter.dla_opac_ng = EXT:dla_opac_ng/Resources/Public/JavaScript/d
 page.includeJS.nouislider = EXT:dla_opac_ng/Resources/Public/JavaScript/nouislider.js
 page.includeCSS.nouislider =  EXT:dla_opac_ng/Resources/Public/CSS/nouislider.css
 
+page.includeJS.lightbox = EXT:dla_opac_ng/Resources/Public/JavaScript/lightbox.min.js
+page.includeCSS.lightbox =  EXT:dla_opac_ng/Resources/Public/CSS/lightbox.css
+
 #page.includeJS.detailtabs = EXT:dla_opac_ng/Resources/Public/JavaScript/detailtabs.js
 
 page.includeCSS.opac-ng =  EXT:dla_opac_ng/Resources/Public/CSS/opac-ng.css
