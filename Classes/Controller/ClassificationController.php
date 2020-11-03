e41364fbc70877f9935766df0f8c71ff47737885
Christopher Timm <timm@effective-webwork.de>
2020-11-03 17:23:57 +0100

    add classification template variables

--- Classes/Controller/ClassificationController.php
+++ Classes/Controller/ClassificationController.php
@@ -8,9 +8,26 @@ class ClassificationController extends ActionController
 
     /**
      * Index Action.
+     * @param string $record
+     * @param string $uid
      */
-    public function indexAction()
+    public function indexAction(string $record = '', string $uid = '')
     {
+        /** @var \TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface $configurationManager */
 
+        $objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager');
+
+        $configurationManager = $objectManager->get('TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface');
+        $settings = $configurationManager->getConfiguration(
+            \TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT,
+            'find',
+            'find'
+        );
+
+        $extSettings = $settings['plugin.']['tx_find.']['settings.'];
+
+        $this->view->assign('settings', $extSettings);
+        $this->view->assign('record', $record);
+        $this->view->assign('uid', $uid);
     }
 }
