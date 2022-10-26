cbe4eb4f381ae512a03c7fd7c9607d606ccefada
Christopher Timm <timm@effective-webwork.de>
2022-10-26 18:26:03 +0200

    Add translation for order information and use different information for each listview type

--- Resources/Public/JavaScript/dla-opac-ng.js
+++ Resources/Public/JavaScript/dla-opac-ng.js
@@ -144,7 +144,7 @@ $(document).ready(function(){
             .done(function( data ) {
                 $('.order-overlay .login-form').hide();
                 $('.order-overlay .info-no-account').hide();
-                $('.order-overlay .info').text($(data).filter("#meldung").text()).show();
+                // $('.order-overlay .info').text($(data).filter("#meldung").text()).show();
                 if ($(data).filter("#meldung").text() == 'Ihre Leihscheine wurden gedruckt') {
                     // var additionalText = 'Ihre Bestellung wurde verschickt';
                     if ($('.field-listview_type').text().trim() == 'Bilder und Objekte') {
