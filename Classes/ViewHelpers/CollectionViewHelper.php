546da12413627933bce64fb5870d932b4070658e
Christopher Timm <timm@effective-webwork.de>
2024-12-01 18:09:54 +0100

    Bugfix incorrect allocation of copies

--- Classes/ViewHelpers/CollectionViewHelper.php
+++ Classes/ViewHelpers/CollectionViewHelper.php
@@ -28,37 +28,47 @@ class CollectionViewHelper extends AbstractViewHelper
         $record = $this->arguments['record'];
         $arrayData = [];
         if ($record) {
-            $url = $protocol . $_SERVER['HTTP_HOST'] . '/?collection=1&type=collection&action=getAllParents&nodeid=' . $record;
-            try {
-                $jsonData = file_get_contents($url);
-                $arrayData = json_decode($jsonData);
-            } catch (\Exception $exception) {
-
+            if (strpos($record, '␝') === false) {
+                $records[0] = $record;
+            } else {
+                $records = explode('␝', $record);
             }
 
-            if ($arrayData) {
-                foreach ($arrayData as $key => $value) {
-                    $breadcrumbArray[0] = [
-                        'record_id' => $value->record_id,
-                        'uid' => $value->uid,
-                        'treeview_title' => $value->treeview_title,
-                    ];
-                    $childObject = $value;
-                    $i = 1;
-                    while ($childObject->child) {
+            foreach ($records as $record) {
+                $url = $protocol . $_SERVER['HTTP_HOST'] . '/?collection=1&type=collection&action=getAllParents&nodeid=' . $record;
+                try {
+                    $jsonData = file_get_contents($url);
+                    $arrayData[] = json_decode($jsonData);
+                } catch (\Exception $exception) {
 
-                        $breadcrumbArray[$i] = [
-                            'record_id' => $childObject->child->record_id,
-                            'uid' => $childObject->child->uid,
-                            'treeview_title' => $childObject->child->treeview_title,
+                }
+            }
+
+            if (!empty($arrayData[0])) {
+                foreach ($arrayData as $recordJsonData) {
+                    foreach ($recordJsonData as $value) {
+                        $breadcrumbArray[0] = [
+                            'record_id' => $value->record_id,
+                            'uid' => $value->uid,
+                            'treeview_title' => $value->treeview_title,
                         ];
-                        $i++;
-                        if (empty($childObject->child->child)) {
-                            break;
+                        $childObject = $value;
+                        $i = 1;
+                        while ($childObject->child) {
+
+                            $breadcrumbArray[$i] = [
+                                'record_id' => $childObject->child->record_id,
+                                'uid' => $childObject->child->uid,
+                                'treeview_title' => $childObject->child->treeview_title,
+                            ];
+                            $i++;
+                            if (empty($childObject->child->child)) {
+                                break;
+                            }
+                            $childObject = $childObject->child;
                         }
-                        $childObject = $childObject->child;
+                        $resultValue[] = $breadcrumbArray;
                     }
-                    $resultValue[] = $breadcrumbArray;
                 }
             } else {
                 $resultValue = NULL;
